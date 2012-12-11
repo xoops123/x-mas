faf163ae6f962eb4bb383400f9f3335615901486
marine <marine@mistgreen.net>
2012-12-11 21:56:04 +0900

    first commit

---
+++ html/themes/x-mas/language/english.php
@@ -0,0 +1,24 @@
+<?php
+//global
+define('_THEME_LANGUAGE_EVENT_INFO', 'event info');
+define('_THEME_LANGUAGE_WELCOME', 'welcome');
+define('_THEME_LANGUAGE_WELCOME_2', '');
+define('_THEME_LANGUAGE_HOME_NAME', 'home');
+define('_THEME_LANGUAGE_LOGIN', 'login：');
+define('_THEME_LANGUAGE_LOGIN_BUTTON', ' OK ');
+define('_THEME_LANGUAGE_LOGIN_FORM', ' login ');
+define('_THEME_LANGUAGE_PWORD', 'password：');
+define('_THEME_LANGUAGE_ADMIN_PAGE', 'admin page');
+define('_THEME_LANGUAGE_LOGOUT', 'logout');
+define('_THEME_LANGUAGE_LOST_PASS', 'lost password');
+define('_THEME_LANGUAGE_RESISTER', 'resister?');
+define('_THEME_LANGUAGE_REM', 'rem');
+define('_THEME_LANGUAGE_NORTIFICATION', 'nortification');
+define('_THEME_LANGUAGE_USER_ACCOUNT', 'user account');
+define('_THEME_LANGUAGE_EDIT_ACCOUNT', 'edit account');
+define('_THEME_LANGUAGE_VIEWPMSG', 'view pm msg');
+define('_THEME_LANGUAGE_USER_NAME', 'user name');
+define('_THEME_LANGUAGE_NOSCRIPT', 'pleas use Javascript');
+define('_THEME_LANGUAGE_SEARCH', 'search');
+
+?>
